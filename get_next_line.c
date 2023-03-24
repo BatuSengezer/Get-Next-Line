2fb07d77661db3fc69f0582b0742625480e41707
BatuSengezer <bsengezer@ku.edu.tr>
2023-03-24 12:25:19 +0100

    ongoing

---
+++ get_next_line.c
@@ -0,0 +1,26 @@
+#include "get_next_line.h"
+
+char* get_next_line(int fd)
+{
+    char* buf = (char*)malloc(100);
+    char* tmp = buf;
+    int bytes_read;
+
+    while ((bytes_read = read(fd, buf, 1)) > 0)
+    {
+        if (*buf == '\n' || *buf == EOF)
+        {
+            break;
+        }
+        buf++;
+    }
+    *buf = '\0';
+    buf = tmp;
+    return buf;
+}
+
+int main()
+{
+    int fd = open("Test123.txt", O_RDONLY);
+    puts(get_next_line(fd));
+}
---
+++ get_next_line_utils.c
@@ -0,0 +1,13 @@
+#include "get_next_line.h"
+
+size_t	ft_strlen(char *s)
+{
+	size_t	i;
+
+	i = 0;
+	if (!s)
+		return (0);
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
